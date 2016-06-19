35befbf6be611869fefe5b7de9dd56cd69976a9b
Adrien Gallou <adriengallou@gmail.com>
2016-06-19 17:00:54 +0200

    use callables to define configuration

--- autoload.php
+++ autoload.php
@@ -6,7 +6,9 @@ use mageekguy\atoum;
 use mageekguy\atoum\scripts;
 
 if (defined('mageekguy\atoum\scripts\runner') === true) {
-    scripts\runner::addAutorunnerConfigurationFile(__DIR__ . DIRECTORY_SEPARATOR . 'configuration.php');
+    scripts\runner::addConfigurationCallable(function($script, $runner) {
+        include __DIR__ . DIRECTORY_SEPARATOR . 'configuration.php';
+    });
 }
 
 
